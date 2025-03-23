9c019195aa90fddcd15b1bdb2900c8eed6bcf557
kaf212 <kaf221122@gmail.com>
2025-03-23 15:03:26 +0100

    required all account attributes in Report model to be positive numbers

--- src/models/Report.js
+++ src/models/Report.js
@@ -6,24 +6,24 @@ const mongoose = require("mongoose")
 
 // **** sub-schemas for nested objects inside the report schema ****
 const liquidAssetsSchema = new mongoose.Schema({
-    total: { type: Number },
-    cash: { type: Number },
-    postal: { type: Number },
-    bank: { type: Number }
+    total: { type: Number, min: 0 },
+    cash: { type: Number, min: 0 },
+    postal: { type: Number, min: 0 },
+    bank: { type: Number, min: 0 }
 }, { _id: false });
 
 const currentAssetsSchema = new mongoose.Schema({
-    total: { type: Number },
+    total: { type: Number, min: 0 },
     liquid_assets: liquidAssetsSchema,
-    receivables: { type: Number },
-    stocks: { type: Number }
+    receivables: { type: Number, min: 0 },
+    stocks: { type: Number, min: 0 }
 }, { _id: false });
 
 const fixedAssetsSchema = new mongoose.Schema({
-    total: { type: Number },
-    machines: { type: Number },
-    movables: { type: Number },
-    real_estate: { type: Number }
+    total: { type: Number, min: 0 },
+    machines: { type: Number, min: 0 },
+    movables: { type: Number, min: 0 },
+    real_estate: { type: Number, min: 0 }
 }, { _id: false });
 
 const activesSchema = new mongoose.Schema({
@@ -32,13 +32,13 @@ const activesSchema = new mongoose.Schema({
 }, { _id: false });
 
 const shortTermDebtSchema = new mongoose.Schema({
-    liabilities: { type: Number }
+    liabilities: { type: Number, min: 0 }
 }, { _id: false });
 
 const longTermDebtSchema = new mongoose.Schema({
-    total: { type: Number },
-    loans: { type: Number },
-    mortgage: { type: Number }
+    total: { type: Number, min: 0 },
+    loans: { type: Number, min: 0 },
+    mortgage: { type: Number, min: 0 }
 }, { _id: false });
 
 const debtSchema = new mongoose.Schema({
@@ -47,10 +47,10 @@ const debtSchema = new mongoose.Schema({
 }, { _id: false });
 
 const equitySchema = new mongoose.Schema({
-    total: { type: Number },
-    shares: { type: Number },
-    legal_reserve: { type: Number },
-    retained_earnings: { type: Number }
+    total: { type: Number, min: 0 },
+    shares: { type: Number, min: 0 },
+    legal_reserve: { type: Number, min: 0 },
+    retained_earnings: { type: Number, min: 0 }
 }, { _id: false });
 
 const passivesSchema = new mongoose.Schema({
@@ -59,21 +59,22 @@ const passivesSchema = new mongoose.Schema({
 }, { _id: false });
 
 const expenseSchema = new mongoose.Schema({
-    total: { type: Number },
-    goods: { type: Number },
-    staff: { type: Number },
-    other_expenses: { type: Number },
-    depreciation: { type: Number },
-    financial: { type: Number },
-    real_estate: { type: Number }
+    total: { type: Number, min: 0 },
+    goods: { type: Number, min: 0 },
+    staff: { type: Number, min: 0 },
+    other_expenses: { type: Number, min: 0 },
+    depreciation: { type: Number, min: 0 },
+    financial: { type: Number, min: 0 },
+    real_estate: { type: Number, min: 0 }
 }, { _id: false });
 
 const earningsSchema = new mongoose.Schema({
-    total: { type: Number },
-    operating_income: { type: Number },
-    financial: { type: Number },
-    real_estate: { type: Number }
+    total: { type: Number, min: 0 },
+    operating_income: { type: Number, min: 0 },
+    financial: { type: Number, min: 0 },
+    real_estate: { type: Number, min: 0 }
 }, { _id: false });
+
 
 const incomeStatementSchema = new mongoose.Schema({
     expense: expenseSchema,
@@ -84,7 +85,7 @@ const incomeStatementSchema = new mongoose.Schema({
 // **** Main report schema ****
 const reportSchema = new mongoose.Schema({
     company_id: { type: String },
-    period: { type: Number },
+    period: { type: Number, required: true},
     balance_sheet: {
         actives: activesSchema,
         passives: passivesSchema
