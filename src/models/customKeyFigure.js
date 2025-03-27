0c1c5b629fe20aa0c5abbf4e3faf88bd47f70287
kaf212 <kaf221122@gmail.com>
2025-03-27 16:44:45 +0100

    corrected attribute naming mistake in customKeyFigure

--- src/models/customKeyFigure.js
+++ src/models/customKeyFigure.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose")
 
 const customKeyFigureSchema = new mongoose.Schema({
     name: {type: String, required: true},
-    password: {type: String}
+    formula: {type: String}
 })
 
 const CustomKeyFigure = mongoose.model('CustomKeyFigure', customKeyFigureSchema, "customKeyFigure")
