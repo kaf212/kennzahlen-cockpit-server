b41aad2f0921e451e53958d3a0fcdd24aa3bb42f
kaf212 <kaf221122@gmail.com>
2025-03-27 22:14:55 +0100

    added GET and POST endpoint for custom key figures

--- src/routes/customKeyFigureRoutes.js
+++ src/routes/customKeyFigureRoutes.js
@@ -6,6 +6,14 @@ const {authenticateAdmin} = require("../middleware/tokenValidation")
 const router = express.Router()
 
 
+async function checkCustomKeyFigureExistenceByName(customKeyFigureName) {
+    const found = await CustomKeyFigure.find({name: customKeyFigureName})
+    if (found.length > 0) {
+        return true
+    }
+    return false
+}
+
 router.get("/", async (req, res, next)=>{
     try {
         const keyFigures = await CustomKeyFigure.find({})
@@ -15,6 +23,39 @@ router.get("/", async (req, res, next)=>{
     }
 
 })
+
+router.get("/:id", async (req, res, next)=> {
+    try {
+        // Source: https://stackoverflow.com/questions/53686554/validate-mongodb-objectid
+        if (!mongoose.Types.ObjectId.isValid(req.params.id)) { // check if objectID is of valid format
+            return res.status(400).json({message: "invalid ID format"})
+        }
+
+        const customKeyFigure = await CustomKeyFigure.findById(req.params.id)
+        if (!customKeyFigure) {
+            return res.status(404).json({message: "custom key figure not found"})
+        }
+        return res.json(customKeyFigure)
+    } catch (err) {
+        next(err)
+    }
+})
+
+
+router.post("/",async (req, res, next)=> {
+    if (await checkCustomKeyFigureExistenceByName(req.body.name)) {
+        return res.status(400).json({message: `custom key figure ${req.body.name} already exists`})
+    }
+    try {
+        const newCustomKeyFigure = new CustomKeyFigure({name: req.body.name, formula: req.body.formula})
+        await newCustomKeyFigure.save()
+        return res.status(201).json({message: "custom key figure created successfully"})
+    } catch (err) { // If mongoose model validation fails
+        return res.status(400).json({message: "Invalid JSON format"})
+    }
+
+})
+
 
 
 module.exports = router
