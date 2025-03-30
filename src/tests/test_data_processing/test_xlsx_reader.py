fe1d36b4ba05e3c648d9b908a1801ecbe062ffc2
kaf212 <kaf221122@gmail.com>
2025-03-30 20:18:58 +0200

    moved data processing tests to /src/tests

--- src/tests/test_data_processing/test_xlsx_reader.py
+++ src/tests/test_data_processing/test_xlsx_reader.py
@@ -1,6 +1,12 @@
 import pytest
-import xlsx_reader
 import json
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../data_processing')))
+
+import xlsx_reader
+
 
 
 def test_1_read_single_report():
