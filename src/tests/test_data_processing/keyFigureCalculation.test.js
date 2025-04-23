7ac5872f278a0b4d5906d0b7761ff252fb570c88
kaf212 <kaf221122@gmail.com>
2025-04-23 17:04:20 +0200

    created test case for equity ratio

--- src/tests/test_data_processing/keyFigureCalculation.test.js
+++ src/tests/test_data_processing/keyFigureCalculation.test.js
@@ -1,71 +1,73 @@
 const mongoose = require('mongoose')
 const Report = require('../../models/Report')
+const {getCurrentKeyFigures} = require("../../data_processing/queries")
+
+const testReport = new Report({
+    company_id: "_testCompany",
+    period: 2024,
+    balance_sheet: {
+        actives: {
+            current_assets: {
+                total: 595,
+                liquid_assets: {
+                    total: 70,
+                    cash: 30,
+                    postal: 25,
+                    bank: 15
+                },
+                receivables: 300,
+                stocks: 225
+            },
+            fixed_assets: {
+                total: 1605,
+                machines: 295,
+                movables: 160,
+                active_real_estate: 1150
+            }
+        },
+        passives: {
+            debt: {
+                total: 1248,
+                short_term: {
+                    total: 348,
+                    liabilities: 348
+                },
+                long_term: {
+                    total: 900,
+                    loans: 200,
+                    mortgage: 700
+                }
+            },
+            equity: {
+                total: 952,
+                shares: 700,
+                legal_reserve: 250,
+                retained_earnings: 2
+            }
+        }
+    },
+    income_statement: {
+        expense: {
+            total: 4287,
+            operating_expense: 1768,
+            staff_expense: 1790,
+            other_expenses: 410,
+            depreciation: 210,
+            financial_expense: 59,
+            real_estate_expense: 50
+        },
+        earnings: {
+            total: 4505,
+            operating_income: 4440,
+            financial_income: 20,
+            real_estate_income: 45
+        }
+    }
+})
 
 
 beforeAll(async () => {
     await mongoose.connect("mongodb://localhost:27017/kennzahlen")
-    const testReport = new Report({
-        company_id: "_testCompany",
-        period: 2024,
-        balance_sheet: {
-            actives: {
-                current_assets: {
-                    total: 595,
-                    liquid_assets: {
-                        total: 70,
-                        cash: 30,
-                        postal: 25,
-                        bank: 15
-                    },
-                    receivables: 300,
-                    stocks: 225
-                },
-                fixed_assets: {
-                    total: 1605,
-                    machines: 295,
-                    movables: 160,
-                    active_real_estate: 1150
-                }
-            },
-            passives: {
-                debt: {
-                    total: 1248,
-                    short_term: {
-                        total: 348,
-                        liabilities: 348
-                    },
-                    long_term: {
-                        total: 900,
-                        loans: 200,
-                        mortgage: 700
-                    }
-                },
-                equity: {
-                    total: 952,
-                    shares: 700,
-                    legal_reserve: 250,
-                    retained_earnings: 2
-                }
-            }
-        },
-        income_statement: {
-            expense: {
-                total: 4287,
-                operating_expense: 1768,
-                staff_expense: 1790,
-                other_expenses: 410,
-                depreciation: 210,
-                financial_expense: 59,
-                real_estate_expense: 50
-            },
-            earnings: {
-                total: 4505,
-                operating_income: 4440,
-                financial_income: 20,
-                real_estate_income: 45
-            }
-        }
-    })
     await testReport.save()
 })
 
@@ -74,10 +76,19 @@ afterAll(async () => {
     await mongoose.disconnect()
 })
 
+/*
+The used test report is partly based on the balance sheet and income statement from chapter 2.11.2.1.
+ */
+
 
 describe("Custom key figure tests", () => {
-    it("Test 1", async () => {
-        const report = await Report.findOne({company_id: "_testCompany"})
-        expect(report).toHaveProperty("company_id", "_testCompany")
+    it("Test equity ratio", async () => {
+        const equity = testReport.balance_sheet.passives.equity.total
+        const totalCapital = testReport.balance_sheet.passives.debt.total + equity
+        const expectedResult = equity / totalCapital
+
+        const calculationResults = await getCurrentKeyFigures(testReport.company_id)
+        expect(calculationResults.keyFigures["equityRatio"]).toEqual(expectedResult)
+
     })
 })
