41e9c470f01b9134550d86ce0234f93b4e1d01bd
kaf212 <kaf221122@gmail.com>
2025-04-24 12:17:42 +0200

    commented db.js

--- scripts/db.js
+++ scripts/db.js
@@ -2,6 +2,10 @@
 const mongoose = require("mongoose")
 
 async function connectDB() {
+    /**
+     * Connects the server to the MongoDB database.
+     * @returns {String} - Success or error message
+     */
     try {
         // Source: https://www.geeksforgeeks.org/how-to-connect-node-js-to-a-mongodb-database/
         await mongoose.connect("mongodb://localhost:27017/kennzahlen", {
