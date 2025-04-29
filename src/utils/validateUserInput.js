7fc3bd92b81bf65a53a318b938a2cfea1c5bf7ef
kaf212 <kaf221122@gmail.com>
2025-04-29 11:26:08 +0200

    fix faulty regex pattern

--- src/utils/validateUserInput.js
+++ src/utils/validateUserInput.js
@@ -7,7 +7,7 @@ function validateInput(input) {
      * @param {String} input - The user input from a request
      * @returns {Boolean} True, if the input is valid, falso otherwise
      */
-    const regex = /^[A-Za-z0-9\s!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~]*$/;
+    const regex = /^[a-zA-Z0-9\s\-_,.()]+$/;
     return regex.test(input);
 }
 
